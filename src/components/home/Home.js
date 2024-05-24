d76ba4375ec1555aa0ac41727af3fabc19b01666
Paul Gerbaux <128037073+Sang-loi@users.noreply.github.com>
2024-05-24 15:03:01 +0200

    Update Home.js TAM

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -17,7 +17,7 @@ export default function Home() {
          <Box>
             <h1>Bonjour, je suis Paul <span className={Style.hand}>🤚</span>
             </h1>
-            <h2>{info.position}.</h2>
+            <h2>{info.position}</h2>
             <Box component={'ul'} p={'0.8rem'}>
                {info.miniBio.map((bio, index) => (
                   <EmojiBullet key={index} emoji={bio.emoji} text={bio.text}/>
